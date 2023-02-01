4c9ec6ef525caa4ed0f6869145546698ef5c9af2
Danny Bermudez <danny@Dannys-MacBook-Pro.local>
2023-01-31 16:36:25 -0800

    api input

--- youview-app/src/Server/API.jsx
+++ youview-app/src/Server/API.jsx
@@ -8,7 +8,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+    'X-RapidAPI-Key': 'ba587c60cbmsh33b9ccb644d1f2bp181a77jsn047fce048636',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
